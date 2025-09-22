17f708b460411fe203c5d604337cbd800be280cd
ClausCode <nclaus.code@gmail.com>
2025-09-22 12:41:53 +0300

    fix ac block

--- src/blocks/advantages-challenges-block/ui/ac-default.tsx
+++ src/blocks/advantages-challenges-block/ui/ac-default.tsx
@@ -17,22 +17,22 @@ export function ACDefault({ fields }: Props) {
               {fields.advantages.description}
             </p>
           )}
-          <div className="grid grid-cols-2 gap-32 justify-between mt-8">
-            <div className="flex justify-start">
+          <div className="flex gap-32 justify-between mt-8">
+            <div className="flex-1 aspect-square relative">
               {fields.advantages.img ? (
                 <Image
                   src={fields.advantages.img.url!}
-                  width={512}
-                  height={512}
+                  fill
                   alt={fields.advantages.img.alt || ""}
+                  className="object-contain"
                 />
               ) : null}
             </div>
-            <div className="grid gap-6 w-4/5">
+            <div className="flex-1 flex flex-col gap-6 w-4/5">
               {fields.advantages.advantages.map((advantage, index) => (
                 <div key={index} className="flex gap-2">
                   <Check className="size-10 text-accent" />
-                  <div className="flex flex-col items-center gap-2">
+                  <div className="flex flex-col items-start gap-2">
                     <span className="text-2xl">{advantage.heading}</span>
                     {advantage.description && (
                       <p className="text-xl text-gray-600">
@@ -46,20 +46,20 @@ export function ACDefault({ fields }: Props) {
           </div>
         </div>
         <div className="flex flex-col gap-4">
-          <div className="w-full flex flex-col items-end">
-            <span className="text-4xl">{fields.advantages.heading}</span>
+          <div className="w-full flex flex-col items-end gap-4">
+            <span className="text-4xl">{fields.challenges.heading}</span>
             {fields.challenges.description && (
               <p className="text-xl text-gray-600">
                 {fields.challenges.description}
               </p>
             )}
           </div>
-          <div className="grid grid-cols-2 gap-32 justify-between mt-8">
-            <div className="grid gap-6 w-4/5">
+          <div className="flex gap-32 justify-between mt-8">
+            <div className="flex-1 flex flex-col gap-6 w-4/5">
               {fields.challenges.challenges.map((challenge, index) => (
                 <div key={index} className="flex gap-2">
                   <Check className="size-10 text-accent" />
-                  <div className="flex flex-col items-center gap-2">
+                  <div className="flex flex-col items-start gap-2">
                     <span className="text-2xl">{challenge.heading}</span>
                     {challenge.description && (
                       <p className="text-xl text-gray-600">
@@ -70,13 +70,13 @@ export function ACDefault({ fields }: Props) {
                 </div>
               ))}
             </div>
-            <div className="flex justify-end">
-              {fields.challenges.img ? (
+            <div className="flex-1 aspect-square relative">
+              {fields.advantages.img ? (
                 <Image
-                  src={fields.challenges.img.url!}
-                  width={512}
-                  height={512}
-                  alt={fields.challenges.img.alt || ""}
+                  src={fields.advantages.img.url!}
+                  fill
+                  alt={fields.challenges?.img?.alt || ""}
+                  className="object-contain"
                 />
               ) : null}
             </div>
@@ -94,23 +94,23 @@ export function ACDefault({ fields }: Props) {
           x1="0%"
           y1="0%"
           x2="40%"
-          y2="100"
+          y2="50"
           strokeOpacity="0.5"
           strokeWidth="4"
           strokeDasharray="8 8"
         />
         <line
           x1="40%"
-          y1="100"
+          y1="50"
           x2="50%"
-          y2="450"
+          y2="150"
           strokeOpacity="0.5"
           strokeWidth="4"
           strokeDasharray="8 8"
         />
         <line
           x1="50%"
-          y1="450"
+          y1="150"
           x2="50%"
           y2="50%"
           strokeOpacity="0.5"
@@ -129,17 +129,8 @@ export function ACDefault({ fields }: Props) {
         <line
           x1="50%"
           y1="50%"
-          x2="10%"
+          x2="0%"
           y2="57%"
-          strokeOpacity="0.5"
-          strokeWidth="4"
-          strokeDasharray="8 8"
-        />
-        <line
-          x1="10%"
-          y1="57%"
-          x2="10%"
-          y2="100%"
           strokeOpacity="0.5"
           strokeWidth="4"
           strokeDasharray="8 8"

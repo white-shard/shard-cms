95d3d69d2f328c321504a4e973901a9a77753101
ClausCode <nclaus.code@gmail.com>
2025-09-26 03:46:14 +0300

    fix form

--- src/lib/hooks/use-dialog.tsx
+++ src/lib/hooks/use-dialog.tsx
@@ -10,6 +10,8 @@ import {
 import { FormComponent } from "@/components/ui/form"
 import { Form } from "@/payload-types"
 import { useState } from "react"
+import cover from "public/form-cover.jpg"
+import Image from "next/image"
 
 export function useDialogWithForm(form?: Form) {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -19,20 +21,24 @@ export function useDialogWithForm(form?: Form) {
 
   const dialog = (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="w-[95vw] max-w-[600px] max-h-[90vh] overflow-y-auto">
-        <div className="w-full h-128 bg-red-300"></div>
-        <DialogHeader>
-          <DialogTitle className="font-normal text-xl sm:text-2xl lg:text-3xl text-center">
-            {form?.heading}
-          </DialogTitle>
-          {form?.description && (
-            <DialogDescription className="text-sm sm:text-base lg:text-lg text-center">
-              {form.description}
-            </DialogDescription>
-          )}
-        </DialogHeader>
+      <DialogContent className="w-[95vw] max-w-[600px] max-h-[90vh] overflow-y-auto px-0 pt-0">
+        <div className="w-full h-72 relative">
+          <Image src={cover} fill alt="" />
+        </div>
+        <div className="px-8 py-4">
+          <DialogHeader>
+            <DialogTitle className="font-normal text-xl sm:text-2xl lg:text-3xl text-center">
+              {form?.heading}
+            </DialogTitle>
+            {form?.description && (
+              <DialogDescription className="text-sm sm:text-base lg:text-lg text-center">
+                {form.description}
+              </DialogDescription>
+            )}
+          </DialogHeader>
 
-        {form && <FormComponent form={form} onClose={closeDialog} />}
+          {form && <FormComponent form={form} onClose={closeDialog} />}
+        </div>
       </DialogContent>
     </Dialog>
   )

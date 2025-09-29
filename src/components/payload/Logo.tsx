d36c4a08535eb2b5b3fe70dffdadcab90f0f9b52
ClausCode <nclaus.code@gmail.com>
2025-09-29 12:19:56 +0300

    fix logo

--- src/components/payload/Logo.tsx
+++ src/components/payload/Logo.tsx
@@ -1,20 +1,15 @@
-import logoDark from "@/assets/logo-dark.png"
 import logoLight from "@/assets/logo-light.png"
 import Image from "next/image"
 
 export default function Logo() {
   return (
     <div className="relative">
-      <Image
-        className="h-20 object-contain block dark:hidden [&[data-theme=dark]]:hidden"
-        src={logoLight}
-        alt="Logo"
-      />
-      <Image
+      <Image className="h-20 object-contain block" src={logoLight} alt="Logo" />
+      {/* <Image
         className="h-20 object-contain hidden dark:block [&[data-theme=dark]]:block absolute top-0 left-0"
         src={logoDark}
         alt="Logo"
-      />
+      /> */}
     </div>
   )
 }
